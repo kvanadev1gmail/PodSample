f678afe3cc061bfce780664d4e44337c3318118b
kvanadev1 <https://kvanadev1@bitbucket.org>
2016-02-10 14:57:06 +0530

    initaial commit

---
+++ PodCreting/PodCreting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodCreting
+//
+//  Created by kvana inc on 10/02/16.
+//  Copyright © 2016 Kvana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodCreting/PodCreting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodCreting
+//
+//  Created by kvana inc on 10/02/16.
+//  Copyright © 2016 Kvana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
